9023e3d945027d0b60cc4253371923e0a1bb4a29
Giovanni <lugk4l@gmail.com>
2021-03-17 19:07:21 -0400

    virtual method added needs heroku deployment

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -1,40 +1,57 @@
 const mongoose = require("mongoose");
 
-const workoutSchema = new mongoose.Schema({
-  day: {
-    type: Date,
-    default: () => new Date(),
-  },
-  exercises: [
-    {
-      type: {
-        type: String,
-        required: [true, "Insert activity type!"],
-      },
-      name: {
-        type: String,
-        required: [true, "Insert activity name!"],
-      },
-      duration: {
-        type: Number,
-        required: [true, "Insert activity duration!"],
-      },
-      weight: {
-        type: Number,
-      },
-      reps: {
-        type: Number,
-      },
-      sets: {
-        type: Number,
-      },
-      distance: {
-        type: Number,
-      },
+const workoutSchema = new mongoose.Schema(
+  {
+    day: {
+      type: Date,
+      default: () => new Date(),
     },
-  ],
-});
-
+    exercises: [
+      {
+        type: {
+          type: String,
+          required: [true, "Insert activity type!"],
+        },
+        name: {
+          type: String,
+          required: [true, "Insert activity name!"],
+        },
+        duration: {
+          type: Number,
+          required: [true, "Insert activity duration!"],
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+  }
+);
 const Workout = mongoose.model("Workout", workoutSchema);
+
+workoutSchema.virtual("totalDuration").get(function () {
+  this.totalDuration = 0;
+  this.exercises.map((el) => {
+    this.totalDuration += Number(el.duration);
+  });
+  return this.totalDuration;
+});
 
 module.exports = Workout;
